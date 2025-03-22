61d405a13f905e75a5f856c5facc9e6d998b2fb9
BBeerBear <xiongx659@gmail.com>
2025-03-21 23:24:16 -0400

    shape the data to return

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -8,8 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AuctionDbContext>(opt =>
 {
-  opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
